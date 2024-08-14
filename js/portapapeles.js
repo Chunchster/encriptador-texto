bf39bc8c531a38c13312331f2fdf1fedba8a79ae
Chunchster <86860235+Chunchster@users.noreply.github.com>
2024-08-14 00:41:13 -0500

    Update portapapeles.js

--- js/portapapeles.js
+++ js/portapapeles.js
@@ -2,7 +2,8 @@ document.getElementById('bPortapapeles').addEventListener('click', function() {
     const textToCopy = document.getElementById('text-result').innerText;
 
     navigator.clipboard.writeText(textToCopy).then(function() {
-        alert('Texto copiado al portapapeles');
+        console.log('Texto copiado al portapapeles');
+        //alert('Texto copiado al portapapeles');
     }).catch(function(err) {
         console.error('Error al copiar el texto: ', err);
     });
